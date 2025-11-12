27c57fd26f670e3d1738bf4209385245b954a4d8
Jon Musker <Jon@Muskernet.com>
2025-11-12 18:48:18 +0000

    update4

--- rmg-demo/main.tf
+++ rmg-demo/main.tf
@@ -22,13 +22,13 @@ resource "sca_policy" "rmg-demo-az-muskernetO365" {
   csp          = "AZURE"
   roles        = [
      {
-      entity_id        = "providers/Microsoft.Authorization/roleDefinitions/b24988ac-6180-42a0-ab88-20f7382dd24c"   #Contributor
+      entity_id        = "subscriptions/78e4f18f-7011-4870-af85-e5c33d019b63/providers/Microsoft.Authorization/roleDefinitions/b24988ac-6180-42a0-ab88-20f7382dd24c"   #Contributor
       entity_source_id = "subscriptions/78e4f18f-7011-4870-af85-e5c33d019b63"
       workspace_type   = "subscription"
       organization_id  = "d8a8541b-d35c-4c6b-9be7-553bd08213d9"
     },
     {
-      entity_id        = "providers/Microsoft.Authorization/roleDefinitions/acdd72a7-3385-48ef-bd42-f606fba81ae7"   #Reader
+      entity_id        = "subscriptions/78e4f18f-7011-4870-af85-e5c33d019b63/providers/Microsoft.Authorization/roleDefinitions/acdd72a7-3385-48ef-bd42-f606fba81ae7"   #Reader
       entity_source_id = "subscriptions/78e4f18f-7011-4870-af85-e5c33d019b63"
       workspace_type   = "subscription"
       organization_id  = "d8a8541b-d35c-4c6b-9be7-553bd08213d9"
@@ -54,6 +54,4 @@ resource "sca_policy" "rmg-demo-az-muskernetO365" {
     max_session_duration = 1
     time_zone            = "Europe/London"
   }
-  end_date     = "2026-01-12T00:00:00.000Z"
-  start_date   = "2025-11-13T00:00:00.000Z"
 }
