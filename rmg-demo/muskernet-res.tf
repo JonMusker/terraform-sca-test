74d5ce16f28ebcc05c1090f7d3296480dd76bfdf
Jon Musker <jon@muskernet.com>
2025-11-12 12:26:38 +0000

    update6

---
+++ rmg-demo/muskernet-res.tf
@@ -0,0 +1,39 @@
+resource "sca_policy" "rmg-demo-az-muskernet" {
+  name         = "RMG-Demo-Azure-Muskernet"
+  description  = "Grants access to roles for subscriptions in Azure tenant Muskernet"
+  csp          = "AZURE"
+  roles        = [
+     {
+      entity_id        = "/subscriptions/78e4f18f-7011-4870-af85-e5c33d019b63/providers/Microsoft.Authorization/roleDefinitions/b24988ac-6180-42a0-ab88-20f7382dd24c"   #Contributor
+      entity_source_id = "subscriptions/78e4f18f-7011-4870-af85-e5c33d019b63"
+      workspace_type   = "subscription"
+      organization_id  = "d8a8541b-d35c-4c6b-9be7-553bd08213d9"
+    },
+    {
+      entity_id        = "/subscriptions/78e4f18f-7011-4870-af85-e5c33d019b63/providers/Microsoft.Authorization/roleDefinitions/acdd72a7-3385-48ef-bd42-f606fba81ae7"   #Reader
+      entity_source_id = "subscriptions/78e4f18f-7011-4870-af85-e5c33d019b63"
+      workspace_type   = "subscription"
+      organization_id  = "d8a8541b-d35c-4c6b-9be7-553bd08213d9"
+    }
+  ]
+  identities   = [
+    {
+      entity_name       = "ACME Cloud Admins"
+      entity_source_id  = "id"
+      entity_class      = "role"
+    }
+  ]
+  access_rules = {
+    days = [
+      "monday",
+      "tuesday",
+      "wednesday",
+      "thursday",
+      "friday"
+    ]
+    from_time            = "09:00"
+    to_time              = "19:00"
+    max_session_duration = 2
+    time_zone            = "Europe/London"
+  }
+}
--- rmg-demo/terraform.auto.tfvars
+++ rmg-demo/terraform.auto.tfvars
@@ -1,4 +1,4 @@
-auth_url                = "https://aax4093.id.cyberark.cloud/oauth2/token/api-test"
+auth_url                = "https://aax4093.id.cyberark.cloud/oauth2/token/oauthsrvsca"
 tenant_domain_name      = "cctest1"
 platform_domain_name    = "cyberark.cloud"
 username                = "api-terraform-1@cyberark.cloud.10061"
